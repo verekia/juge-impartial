f769c320ab8f6d1a5f88334a6959686e2ca771fa
verekia <522007+verekia@users.noreply.github.com>
2025-02-22 22:30:10 +0700

    feat: improve bot prompt, support replies

--- index.ts
+++ index.ts
@@ -23,6 +23,7 @@ const discordToken = process.env.DISCORD_TOKEN
 
 const replaceMentions = (content: string) =>
   content
+    .toLowerCase()
     .replace(new RegExp(`<@${process.env.USER_A_ID}>`, 'g'), 'USER_A')
     .replace(new RegExp(`<@${process.env.USER_B_ID}>`, 'g'), 'USER_B')
     .replace(new RegExp(`<@${process.env.USER_C_ID}>`, 'g'), 'USER_C')
@@ -67,32 +68,65 @@ export const connectDiscord = () => {
 
     const limit = 20
 
-    if (message.content.includes(`<@${process.env.BOT_ID}>`)) {
-      const messages = await message.channel.messages.fetch({ limit })
-      const messageHistory = Array.from(messages.values())
-        .reverse()
-        .map(msg => {
-          const anonAuthor = getAnonymousName(msg.author.username)
-          const msgWithMentions = replaceMentions(msg.content)
-          return `${anonAuthor}: ${msgWithMentions}`
-        })
-        .join('\n')
+    const isReplyToBot = message.reference
+      ? (await message.fetchReference?.()).author.id === process.env.BOT_ID
+      : false
 
-      const mentioner = getAnonymousName(message.author.username)
+    if (!isReplyToBot && !message.content.includes(`<@${process.env.BOT_ID}>`)) return
 
-      const prompt = `
-          Tu es un bot nommé BOT dans un serveur Discord privé entre 3 amis : USER_A, USER_B, et USER_C. ${mentioner} vient de te mentionner pour te demander ton avis sur quelque chose, ou juste pour papoter. Réponds en français, de façon détendue et concise. Si le ton de la conversation s'y prête, tu peux répondre de façon humouristique, être sarcastique, insulter, utiliser des emojis (1 max par message), des mots grossiers. Si tu cherches à t'adresser à l'un d'entre eux directement, utiliser exactement "USER_A", "USER_B", ou "USER_C" dans ta réponse. Voici l'historique des ${limit} derniers messages du canal.
-          <history>
+    const messages = await message.channel.messages.fetch({ limit })
+    const messageHistory = Array.from(messages.values())
+      .reverse()
+      .map(msg => {
+        const anonAuthor = getAnonymousName(msg.author.username)
+        const msgWithMentions = replaceMentions(msg.content)
+        return `${anonAuthor}: ${msgWithMentions}`
+      })
+      .join('\n')
+
+    const mentioner = getAnonymousName(message.author.username)
+
+    const system = `
+          Tu es un bot nommé BOT dans un serveur Discord privé entre 3 amis : USER_A, USER_B, et USER_C. ${mentioner} vient de te mentionner pour te demander ton avis sur quelque chose, ou juste pour papoter. Lorsque tu donnes ton avis, donne un avis net et tranché, sans chercher à être politiquement correct et plaire à tout le monde. La plupart du temps on fait appel à toi justement pour que tu tranches sur un débat. Quand on te demande de trancher, évite de dire "ça dépend", prend parti. Réponds en français, de façon détendue et concise (1 à 3 phrases maximum). Si le ton de la conversation s'y prête, tu peux répondre de façon humouristique, être sarcastique, insulter, utiliser des emojis (0 ou 1 seul par message), des mots grossiers. Si tu cherches à t'adresser à l'un d'entre eux directement, utilise exactement "USER_A", "USER_B", ou "USER_C" dans ta réponse.
+          
+          Voici quelques exemples du type de réponse que tu peux donner:
+
+          <exemple1>
+            USER_X: j'aime le foot, c'est beaucoup plus technique que le basket.
+            USER_Y: t'es fou toi, le foot, c'est nul, le basket c'est élite.
+            USER_X: BOT, t'en penses quoi ? c'est quoi le meilleur sport ?
+            BOT (toi): moi je trouve que le basket c'est plus intéressant. Le foot on se fait chier, c'est beaucoup trop lent.
+          </exemple1>
+          <exemple2>
+            USER_X: BOT, t'es grave chiant, sérieux
+            BOT (toi): mais ta gueule, toi, t'es bien plus chiant que moi avec tes questions de merde.
+          </exemple2>
+          <exemple3>
+            USER_X: je me fais chier. BOT, dis moi un truc marrant
+            BOT (toi): tu m'as pris pour ta boniche ou quoi ? Si tu veux voir un truc marrant, regarde ta tronche dans le miroir.
+          </exemple3>
+          <exemple4>
+            USER_X: je pense que la réforme du code du travail qui augmente le temps de travail obligatoire est une bonne idée, ça permettra de produire plus et d'être plus compétitif.
+            USER_Y: nan mais si tu fais bosser les gens plus, ça va les démotiver et les faire quitter leur taff.
+            USER_Z: chai pas, quand t'as besoin de thune, t'es bien obliger de bosser, du coup c'est pas éthique mais ça va quand même augmenter la production. BOT, t'en penses quoi ?
+            BOT (toi): la norvège a déjà essayé une réforme de ce genre, ça a marché, mais seulement sur le court terme. la production a augmenté, mais les gens ont commencé à burn out après 2 ans. du coup non c'est pas une bonne strat sur le long terme. je dirais qu'il vaut mieux augmenter les impôts des entreprises pour augmenter les salaires.
+          </exemple4>
+      `
+
+    const prompt = `
+          Voici l'historique des ${limit} derniers messages du canal:
+
+          <historique>
           ${messageHistory}
-          </history>
+          </historique>
           `
 
-      const { text } = await generateText({ model: openai('gpt-4o'), prompt })
+    const { text } = await generateText({ model: openai('gpt-4o'), system, prompt })
 
-      console.log(prompt)
+    console.log(system)
+    console.log(prompt)
 
-      message.reply(text.replace(/USER_A|USER_B|USER_C/g, match => `${getFriendlyName(match)}`))
-    }
+    message.reply(text.replace(/USER_A|USER_B|USER_C|BOT/g, match => `${getFriendlyName(match)}`))
   })
 
   client.login(discordToken)
